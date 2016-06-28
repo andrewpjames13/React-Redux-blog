6176457048dd4d8bfdba299177a3c01d6e2a9018
andrewpjames13 <andrew.james@acumendigital.com>
2016-06-28 13:00:54 -0600

    Set up fetchPosts action to be called before PostsIndex renders for the first time

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts</div>
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+export default connect(null, { fetchPosts })(PostsIndex);
